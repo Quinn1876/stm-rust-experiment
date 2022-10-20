ceb5f18f02debf89d6b2dcaf7467347f743532bb
Quinn1876 <qhodges0442@gmail.com>
2022-10-19 20:30:03 -0400

    wip on main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry; // The runtime
+use embedded_hal::digital::v2::OutputPin; // the `set_high/low` function
+use stm32f0xx_hal::{delay::Delay, pac, prelude::*}; // stm specific functions
+
+#[allow(unused_imports)]
+use panic_halt; // When a panic occurrs stop the microcontroller
+
+
+// Marks the entry point to the applicaiton
+// Cortex runtime adds some startup code before this to 
+// help get us going
+#[entry]
 fn main() {
-    println!("Hello, world!");
+    
 }
